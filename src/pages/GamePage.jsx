8c149b21b8c7e27303f19372a401d01206ff342e
shuklaksh <shuklaksh156@gmail.com>
2023-05-26 01:00:37 +0530

    feat: game page

---
+++ src/pages/GamePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Game from '../components/gamePage/Game'
+
+function GamePage() {
+  return (
+    <div className='game-page-container'>
+      <Game />
+    </div>
+  )
+}
+
+export default GamePage
